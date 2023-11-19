8ca5fadcaaf29d8f79a6e8e243519fbdebe02de1
Kingsmg6 <151435368+Kingsmg6@users.noreply.github.com>
2023-11-19 04:40:35 -0600

    Update global.php

--- modules/global.php
+++ modules/global.php
@@ -1,5 +1,5 @@
 <?php
-$botToken = "5449842563:AAGgqf0HuPFEuNjIOB_zfUyyk4Wn2gARTVQ"; // Enter ur bot token
+$botToken = "6784022172:AAEuAHmH_dk5IEeVX_6zDl5YBWMhMGTKMco"; // Enter ur bot token
 $website = "https://api.telegram.org/bot".$botToken;
 error_reporting(0);
 $update = file_get_contents('php://input');
